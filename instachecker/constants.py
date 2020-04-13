f703031e27b97d7636376cb321aaf8f192d2a5c1
Patrick Allen <prallen90@gmail.com>
2020-04-13 02:41:09 -0400

    Move command lists to constants.py

---
+++ instachecker/constants.py
@@ -0,0 +1,2 @@
+PAUSE_COMMANDS = ("stop", "pause", "hold", "shut up", "fuck off")
+UNPAUSE_COMMANDS = ("continue", "start", "ready and willing")
--- instachecker/handlers.py
+++ instachecker/handlers.py
@@ -5,6 +5,7 @@ import urllib
 
 import dotenv
 
+from . import constants
 from .checker import SlotChecker
 from .config import stores_from_config
 from .config import twilio_config
@@ -21,7 +22,7 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger("instachecker")
 
 
-def get_store(stores, store_id):
+def _get_store(stores, store_id):
     for store in stores:
         if store.id == store_id:
             return store
@@ -40,16 +41,22 @@ def check_stores(_event=None, _ctx=None):
     for user in users:
         changes = []
         for store_id in user.store_ids:
-            store = get_store(stores, store_id)
+            store = _get_store(stores, store_id)
+
+            if not store:
+                raise ValueError(f"Store with id {store_id} does not exist.")
+
             last_status = status_store.get_status(user.id, store.id)
             current_status = checker.get_status(user, store)
             if last_status != current_status:
                 changes.append(UserStoreStatus(user.id, store.id, current_status))
 
         for change in changes:
-            store = get_store(stores, change.store_id)
+            store = _get_store(stores, change.store_id)
+
             if change.status == "AVAILABLE" and user.messaging_enabled:
                 notifier.notify(user, store)
+
             status_store.set_status(user.id, store.id, change.status)
 
 
@@ -64,27 +71,24 @@ def seed_users(_event, _ctx):
             log.error(str(e))
 
 
-def sms(event, _ctx):
-    body = base64.b64decode(event["body"]).decode("utf-8")
-    message = dict(urllib.parse.parse_qsl(body))
-    print("MESSAGE\n", message)
-    from_number = message["From"]
-    body = message["Body"]
-    print(body)
+def sms(event=None, _ctx=None):
+    content = base64.b64decode(event["body"]).decode("utf-8")
+    message = dict(urllib.parse.parse_qsl(content))
+    body = message["Body"].lower().strip()
 
     user_store = UserStore("UsersTable")
 
-    user = user_store.get(from_number)
+    user = user_store.get(message["From"])
 
     if not user:
         resp = "idkwtfya"
-    elif body.lower().strip() in ["stop", "pause", "hold", "shut up", "fuck off"]:
+    elif body in constants.PAUSE_COMMANDS:
         if not user.messaging_enabled:
             resp = "I got it the first time. I'll continue fucking off."
         else:
             resp = "Roger that. Standing by. Send 'ready and willing' to continue messaging."
         user_store.disable_messaging(user.id)
-    elif body.lower().strip() in ["continue", "start", "ready and willing"]:
+    elif body in constants.UNPAUSE_COMMANDS:
         resp = "I'm on it! Send a friendly 'fuck off' to pause messaging."
         user_store.enable_messaging(user.id)
     else:
@@ -94,7 +98,3 @@ def sms(event, _ctx):
         "statusCode": 200,
         "body": str(resp),
     }
-
-
-if __name__ == "__main__":
-    check_stores()
